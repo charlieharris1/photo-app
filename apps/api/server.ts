989e96379000eb554dee08e638aafcde9a4b8a30
Charlie Harris <charlie.harris@krakenflex.com>
2025-01-13 08:19:13 +0000

    Add Clockwise IP for future ref

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -2,6 +2,7 @@ import app from './src/index';
 
 const PORT = 3000;
 
+// Clockwise IP = '10.19.14.40'
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
